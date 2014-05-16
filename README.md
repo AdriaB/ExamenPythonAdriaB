4c71eb90f58ae5d9fccb42508e08d9dac3e66bac
AdriaB <adria_bier@hotmail.com>
2014-05-16 18:09:33 +0200

    afegida la base de dades de l'Aram Miquel

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ ExamenPythonAdriaB
 ==================
 
 Examen M05 Adrià Biarnés
+
+He utilitzt les dades de prova de la base de dades de l'Aram Miquel
+També he utilitzat el seu mètode per a llistar Apps (de pago o no)
--- model/aplicacion.py
+++ model/aplicacion.py
@@ -11,3 +11,32 @@ class Aplicacion:
         self.puntuacion = puntuacion
         self.comentarios = comentarios
 
+    def getName(self):
+        return self.nombre
+    def getDeveloper(self):
+        return self.proveedor
+    def getDate(self):
+        return self.fecha
+    def getPrice(self):
+        return self.precio
+    def getNumberDownloads(self):
+        return self.descargas
+    def getNumberScores(self):
+        return self.puntuaciones
+    def getScore(self):
+        return self.puntuacion
+    def getNumberComments(self):
+        return self.comentarios
+
+    def insertToDb(self):
+        appLine = self.nombre + ";" + self.proveedor + ";" + self.fecha + ";" + str(self.precio) + ";" + str(self.descargas) + ";" + str(self.puntuaciones) + ";" + str(self.puntuacion) + ";" + str(self.comentarios)
+        #database = DbController()
+        #database.insertLine(appLine)
+
+    def calcTotalMoneyEarned(self):
+
+        totalMoney = float(self.price) * float(self.numberDownloads)
+        if self.price == 0:
+            print("Free apps don't have this option!")
+        else:
+            print("Total money earned with selled: " + str(totalMoney))
