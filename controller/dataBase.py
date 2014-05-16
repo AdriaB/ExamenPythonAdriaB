3b4d46725f355b220b6a234d774e272746a8b387
AdriaB <adria_bier@hotmail.com>
2014-05-16 17:14:35 +0200

    Afegida l'opció per a sumar un comentari a una aplicació

--- controller/dataBase.py
+++ controller/dataBase.py
@@ -8,11 +8,11 @@ import datetime
 
 class DataBase:
     def __init__(self, rutafitxer="../database/aplicacions.txt"):
-        """Iniciem la classe amb al ruta de la base de dades"""
+        """Iniciamos la classe con al ruta de la base de datos"""
         self.rutafitxer = rutafitxer
 
     def afegeixAplicacio(self, aplicacion):
-        """Afegeix una aplicacio al fitxer d'aplicacions"""
+        """Añade una aplicación al fichero de aplicaciones"""
         added = False
         exists = False
         if os.path.isfile(self.rutafitxer):
@@ -27,6 +27,27 @@ class DataBase:
                     f.write(aplicacion.nombre+";"+aplicacion.proveedor+";"+aplicacion.fecha+";"+str(aplicacion.precio)+";"+str(aplicacion.descargas)+";"+str(aplicacion.puntuaciones)+";"+str(aplicacion.puntuacion)+";"+str(aplicacion.comentarios)+";\n")
                     added = True
         else:
-            print("Error! couldn't find apps' file")
+            print("Error! No se ha podido encontrar el fichero de aplicaciones!")
 
         return added
+
+    def sumarDescarga(self, nombre):
+        """Suma una descarga a la aplicación con ese nombre. Devuelve True o False en funcion de si se ha encontrado (y modificado) o no"""
+        if os.path.isfile(self.rutafitxer):
+            file = open(self.rutafitxer, 'r')
+            llista = file.readlines()
+            file.close()
+            trobat = False
+            with open(self.rutafitxer, 'w') as file:
+                for linia in llista:
+                    if linia.split(";")[0] != nombre:
+                        file.write(linia)
+                    else:
+                        linia1 = linia.split(";")[:-1]
+                        descargues = int(linia.split(";")[4])
+                        resultat = linia1[0]+";"+linia1[1]+";"+linia1[2]+";"+linia1[3]+";"+str(descargues+1)+";"+linia1[5]+";"+linia1[6]+";"+linia1[7]+";\n"
+                        file.write(resultat)
+                        trobat = True
+        else:
+            print("Error! No se ha podido encontrar el fichero de aplicaciones!")
+        return trobat
