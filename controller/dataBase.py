1e4f76e00ff1c2bd24ad911d28fb65583eca36d7
AdriaB <adria_bier@hotmail.com>
2014-05-16 15:48:27 +0200

    Creada l'estructura de classes i carpetes del projecte

---
+++ controller/dataBase.py
@@ -0,0 +1,6 @@
+__author__ = 'adria'
+
+class dataBase:
+    def __init__(self, rutafitxer="../database/aplicacions.txt"):
+        """Iniciem la classe amb al ruta de la base de dades"""
+        self.rutafitxer = rutafitxer
---
+++ model/aplicacion.py
@@ -0,0 +1,13 @@
+__author__ = 'adria'
+
+class Aaplicacion:
+    def __init__(self, nombre, proveedor, fecha, precio, descargas, puntuaciones, puntuacion, comentarios):
+        self.nombre = nombre
+        self.proveedor = proveedor
+        self.fecha = fecha
+        self.precio = precio
+        self.descargas = descargas
+        self.puntuaciones = puntuaciones
+        self.puntuacion = puntuacion
+        self.comentarios = comentarios
+
